ec9f050d25b7fbee5b11ef24557c57388f6b9dc1
Matthew Bodaly <matthewbodaly@gmail.com>
2015-04-24 12:08:58 -0700

    1.1
     added some logging
    - disabled the set computer sleep for now
    - added comments / additional notes

--- logout.sh
+++ logout.sh
@@ -1,14 +1,25 @@
 #!/bin/sh
 #disables sleep
-systemsetup -setcomputersleep Never
+#systemsetup -setcomputersleep 15
 
 #http://hints.macworld.com/article.php?story=20040330161158532
-echo $((`ioreg -c IOHIDSystem | sed -e '/HIDIdleTime/ !{ d' -e 't' -e '}' -e 's/.* = //g' -e 'q'` / 1000000000))
-IDLE="$((`ioreg -c IOHIDSystem | sed -e '/HIDIdleTime/ !{ d' -e 't' -e '}' -e 's/.* = //g' -e 'q'` / 1000000000))"
-if [ "$IDLE" -ge "60
-  " ] ;
+#https://jamfnation.jamfsoftware.com/discussion.html?id=11807
+#Make the directory for the flat file
+#I could add an if/then in here
+mkdir /Library/jamf/
+#This gets the IDLE time for variable
+IDLE=$((`ioreg -c IOHIDSystem | sed -e '/HIDIdleTime/ !{ d' -e 't' -e '}' -e 's/.* = //g' -e 'q'` / 1000000000))
+#Writes the variable to flatfile
+#Next version to delete this file at the beginning of the script so it doesn't grow over time.
+#I made it this way to test... there should be two lines first one is idle when the script ran
+#Second should be super close when the if statement starts
+echo $IDLE >> /Library/jamf/flatfile
+#This is in seconds
+if [ "$IDLE" -ge "3600" ] ;
   then
+  echo $IDLE >> /Library/jamf/flatfile
   osascript -e 'tell application "System Events" to log out'
   pause 90
   killall loginwindow
 fi
+exit 0
