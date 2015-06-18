61b18c2b3fb708b17da41930e99169ec4cd3b232
Owen Pragel <owen.pragel@74bit.com>
2015-06-17 17:05:28 -0700

    Script rewrite, less static, less fluff, renamed.

---
+++ autoUpdateChrome/auto-install-chrome.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+# This script will download and install Google Chrome on a fresh installation of Mac OS X.
+# Usage: curl -fkL gist.github.com/raw/4364590/install-chrome.sh | sh
+
+DOWNLOAD_URL="https://dl.google.com/chrome/mac/stable/GGRO/googlechrome.dmg"
+DMG_PATH="/tmp/Google Chrome.dmg"
+DMG_VOLUME_PATH="/Volumes/Google Chrome/"
+APP_NAME="Google Chrome.app"
+APP_PATH="/Applications/$APP_NAME"
+APP_PROCESS_NAME="Google Chrome"
+APP_INFO_PLIST="Contents/Info.plist"
+
+if pgrep "$APP_PROCESS_NAME" &>/dev/null; then
+  echo 'Error - Chrome is currently running!'
+else
+  curl --retry 3 -L "$DOWNLOAD_URL" -o "$DMG_PATH"
+  hdiutil attach -nobrowse -quiet "$DMG_PATH"
+  version=$(defaults read "$DMG_VOLUME_PATH/$APP_NAME/$APP_INFO_PLIST" CFBundleShortVersionString)
+  printf "Installing $APP_PROCESS_NAME version %s" "$version"
+  rm -rf /Applications/Google\ Chrome.app
+  ditto -rsrc "$DMG_VOLUME_PATH/$APP_NAME" "$APP_PATH"
+  hdiutil detach -quiet "$DMG_VOLUME_PATH"
+  rm "$DMG_PATH"
+fi
