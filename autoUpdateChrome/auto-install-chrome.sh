fe1c1e05220b423622bf0f53d9e04e14d3e7c2b4
Owen Pragel <owen.pragel@74bit.com>
2015-06-17 17:37:48 -0700

    Remove duplicated command
    RM command ran twice.

--- autoUpdateChrome/auto-install-chrome.sh
+++ autoUpdateChrome/auto-install-chrome.sh
@@ -17,7 +17,6 @@ else
   hdiutil attach -nobrowse -quiet "$DMG_PATH"
   version=$(defaults read "$DMG_VOLUME_PATH/$APP_NAME/$APP_INFO_PLIST" CFBundleShortVersionString)
   printf "Installing $APP_PROCESS_NAME version %s" "$version"
-  rm "$DMG_PATH"
   ditto -rsrc "$DMG_VOLUME_PATH/$APP_NAME" "$APP_PATH"
   hdiutil detach -quiet "$DMG_PATH"
   rm "$DMG_PATH"
