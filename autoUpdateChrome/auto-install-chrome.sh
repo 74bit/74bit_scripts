08ade213af091ec58982481411647e041bafc6a3
Owen Pragel <owen.pragel@74bit.com>
2015-08-20 11:35:08 -0700

    Added permission fixes and version key variable

--- autoUpdateChrome/auto-install-chrome.sh
+++ autoUpdateChrome/auto-install-chrome.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-# This script will download and install the latest Google Chrome version on OS X
 
 DOWNLOAD_URL="https://dl.google.com/chrome/mac/stable/GGRO/googlechrome.dmg"
 DMG_PATH="/tmp/Google Chrome.dmg"
@@ -8,15 +7,19 @@ APP_NAME="Google Chrome.app"
 APP_PATH="/Applications/$APP_NAME"
 APP_PROCESS_NAME="Google Chrome"
 APP_INFO_PLIST="Contents/Info.plist"
+APP_VERSION_KEY="CFBundleShortVersionString"
+APP_PERMISSION_USER="root"
+APP_PERMISSION_GROUP="wheel"
 
 if pgrep "$APP_PROCESS_NAME" &>/dev/null; then
   printf "Error - %s is currently running!" "$APP_PROCESS_NAME"
 else
   curl --retry 3 -L "$DOWNLOAD_URL" -o "$DMG_PATH"
   hdiutil attach -nobrowse -quiet "$DMG_PATH"
-  version=$(defaults read "$DMG_VOLUME_PATH/$APP_NAME/$APP_INFO_PLIST" CFBundleShortVersionString)
+  version=$(defaults read "$DMG_VOLUME_PATH/$APP_NAME/$APP_INFO_PLIST" "$APP_VERSION_KEY")
   printf "Installing $APP_PROCESS_NAME version %s" "$version"
   ditto -rsrc "$DMG_VOLUME_PATH/$APP_NAME" "$APP_PATH"
+  chown -R "$APP_PERMISSION_USER":"$APP_PERMISSION_GROUP" "$APP_PATH"
   hdiutil detach -quiet "$DMG_PATH"
   rm "$DMG_PATH"
 fi
